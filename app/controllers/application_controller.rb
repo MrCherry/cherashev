c4433c3b4d40a303c4c937517a0542015e4e323b
Фёдор Черашев <cherashev.f@gmail.com>
2015-01-24 22:40:12 +0300

    Devise routes to custom controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
 
   # before_action :authenticate_user!
 
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      upmin.root_path
+    else
+      edit_user_registration_path
+    end
+  end
 end
