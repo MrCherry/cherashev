f5deb36387ace34306c44e9dba58f09310421d74
Фёдор Черашев <cherashev.f@gmail.com>
2015-01-24 19:30:21 +0300

    User roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ActiveRecord::Base
+  enum role: [:visitor, :admin]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  # rails_admin do
-  #   list do
-  #     field :email
-  #     fields :created_at
-  #   end
-  # end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "roles" do
+    it "returns two roles by default" do
+      expect(User.roles).to eq({"visitor" => 0, "admin" => 1})
+    end
+  end
+
+  context "role" do
+    it "equals :visitor by default" do
+      user = create(:user)
+      expect(user.role).to eq("visitor")
+    end
+
+    it "remembers given :admin role" do
+      user = create(:user)
+      user.role = :admin
+      user.save
+      expect(user.role).to eq("admin")
+    end
+  end
+
+  context "admin? method" do
+    it "exists" do
+      user = create(:user)
+      expect{user.admin?}.not_to raise_error
+    end
+
+    it "returns true if user has got the admin role" do
+      user = create(:user)
+      user.role = :admin
+      expect(user.admin?).to be_truthy
+    end
+
+    it "returns false if user hasn't got the admin role" do
+      user = create(:user)
+      user.role = :visitor
+      expect(user.admin?).to be_falsey
+    end
+  end
+
+  context "visitor? method" do
+    it "exists" do
+      user = create(:user)
+      expect{user.visitor?}.not_to raise_error
+    end
+
+    it "returns true if user has got the visitor role" do
+      user = create(:user)
+      user.role = :visitor
+      expect(user.visitor?).to be_truthy
+    end
+
+    it "returns false if user hasn't got the visitor role" do
+      user = create(:user)
+      user.role = :admin
+      expect(user.visitor?).to be_falsey
+    end
+  end
+
 end
