8d3fa807cf26192616e4786b6481d395804770b7
Фёдор Черашев <cherashev.f@gmail.com>
2015-01-26 04:43:05 +0300

    db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+if Page.all.count == 0
+  31.times do
+    Page.create!(
+        title: Faker::Lorem.sentence,
+        text: Faker::Lorem.paragraphs(rand(5..10)).map{|p| "<p>#{p}</p>"}.join("\n"),
+        category: :blog,
+        state: :published
+    )
+  end
+
+  8.times do
+    Page.create!(
+        title: Faker::Lorem.sentence,
+        text: Faker::Lorem.paragraphs(rand(5..10)).map{|p| "<p>#{p}</p>"}.join("\n"),
+        category: :static,
+        state: :published
+    )
+  end
+end
+
+if User.all.count == 0
+  User.create!(
+      email: 'cherashev.f@gmail.com',
+      password: 'qazwsxedc',
+      password_confirmation: 'qazwsxedc',
+      role: :admin
+  )
+end
+
