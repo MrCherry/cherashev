597f713adbb5978972d574573e1b03496276d6b5
Фёдор Черашев <cherashev.f@gmail.com>
2015-01-26 03:40:15 +0300

    Users factory email fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    email {Faker::Internet.email}
+    email {Faker::Internet.safe_email}
     password = Faker::Internet.password(8)
     password {password}
     password_confirmation {password}
   end
 
   factory :admin_user do
-    email {Faker::Internet.email}
+    email {Faker::Internet.safe_email}
     password = Faker::Internet.password(8)
     password {password}
     password_confirmation {password}
