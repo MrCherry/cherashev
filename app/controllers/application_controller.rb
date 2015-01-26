f71b5006cfa1e37e20bfa7da2eb20a1a3d20f6b2
Фёдор Черашев <cherashev.f@gmail.com>
2015-01-26 03:38:51 +0300

    UI changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # before_action :authenticate_user!
+  rescue_from Exception do |e|
+    redirect_to root_path, flash: {error: 'Упс. Ошибочка вышла...'}
+  end
+
+  rescue_from CanCan::AccessDenied do |e|
+    redirect_to root_path, flash: {error: I18n::t('unauthorized')}
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    redirect_to root_path, flash: {error: 'Зачем искать несуществующее?'}
+  end
 
   def after_sign_in_path_for(resource)
     if resource.admin?
