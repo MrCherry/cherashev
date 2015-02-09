056d3a7c2dadec8c96dfea4acbb6b3bf0ad9cbed
Фёдор Черашев <cherashev.f@gmail.com>
2015-02-09 11:37:42 +0300

    Config / Upload path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.uploads_path = Rails.root.join('public', 'uploads')
 end
