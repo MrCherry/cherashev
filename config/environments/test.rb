2a0c4e23897974036d058827b1dff908a6944536
Фёдор Черашев <fc@voltmobi.com>
2016-02-13 20:07:39 +0300

    Let's try Selenium without Chrome. Do you like it, Travis?

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,6 +16,15 @@ Rails.application.configure do
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
 
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
