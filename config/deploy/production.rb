7edac644fc69d33a6fd8581a6f59977bf9a55b2d
Фёдор Черашев <fc@voltmobi.com>
2016-02-13 11:23:47 +0300

    Minor configs cleanups

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ set :stage, :production
 set :branch, 'master'
 set :rails_env, 'production'
 set :username, 'deployer'
-set :deploy_to, "/home/#{fetch(:username)}/projects/#{fetch(:application)}_#{fetch(:app_version).gsub(".", "_")}/"
+set :deploy_to, "/home/#{fetch(:username)}/#{fetch(:application)}/"
 server 'cherashev.com', user: fetch(:username), roles: %w{web app db}
