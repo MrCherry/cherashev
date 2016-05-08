4be91dedabc40513f21d08dcd95767ef48b5e0cc
Feodor Cherashev <cherashev.f@gmail.com>
2016-05-08 12:08:46 +0300

    Code coverage fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,58 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "roles" do
-    it "returns two roles by default" do
-      expect(User.roles).to eq({"visitor" => 0, "admin" => 1})
+  context 'validations' do
+    it 'validates presence of required attibutes' do
+      is_expected.to validate_presence_of(:email)
+      is_expected.to validate_presence_of(:password)
+    end
+
+    it 'validates email uniqueness' do
+      create(:user)
+      is_expected.to validate_uniqueness_of(:email).case_insensitive
     end
   end
 
-  context "role" do
-    it "equals :visitor by default" do
+  context 'roles' do
+    it 'returns two roles by default' do
+      expect(User.roles).to eq({'visitor' => 0, 'admin' => 1})
+    end
+  end
+
+  context 'role' do
+    it 'equals :visitor by default' do
       user = create(:user)
-      expect(user.role).to eq("visitor")
+      expect(user.role).to eq('visitor')
     end
 
-    it "remembers given :admin role" do
+    it 'remembers given :admin role' do
       user = create(:user)
       user.role = :admin
       user.save
-      expect(user.role).to eq("admin")
+      expect(user.role).to eq('admin')
     end
   end
 
-  context "admin? method" do
-    it "exists" do
+  context 'admin? method' do
+    it 'exists' do
       user = create(:user)
       expect{user.admin?}.not_to raise_error
     end
 
-    it "returns true if user has got the admin role" do
+    it 'returns true if user has got the admin role' do
       user = create(:user)
       user.role = :admin
       expect(user.admin?).to be_truthy
     end
 
-    it "returns false if user hasn't got the admin role" do
+    it 'returns false if user has not got the admin role' do
       user = create(:user)
       user.role = :visitor
       expect(user.admin?).to be_falsey
     end
   end
 
-  context "visitor? method" do
-    it "exists" do
+  context 'visitor? method' do
+    it 'exists' do
       user = create(:user)
       expect{user.visitor?}.not_to raise_error
     end
 
-    it "returns true if user has got the visitor role" do
+    it 'returns true if user has got the visitor role' do
       user = create(:user)
       user.role = :visitor
       expect(user.visitor?).to be_truthy
     end
 
-    it "returns false if user hasn't got the visitor role" do
+    it 'returns false if user has not got the visitor role' do
       user = create(:user)
       user.role = :admin
       expect(user.visitor?).to be_falsey
