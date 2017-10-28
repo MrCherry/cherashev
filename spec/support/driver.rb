06b43fc3bc253697f0713ee333ff397ae9f23fcf
Feodor Cherashev <cherashev.f@gmail.com>
2017-10-28 22:04:55 +0300

    selenium-webdriver chrome download directory, codeclimate reports returned

--- spec/support/driver.rb
+++ spec/support/driver.rb
@@ -15,9 +15,7 @@ Capybara.register_driver :selenium_chrome do |app|
   profile = Selenium::WebDriver::Chrome::Profile.new
 
   # DownloadHelper
-  profile['browser.download.dir'] = DownloadHelper::PATH.to_s
-  profile['browser.download.folderList'] = 2
-  profile['browser.helperApps.neverAsk.saveToDisk'] = 'image/png'
+  profile['download.default_directory'] = DownloadHelper::PATH.to_s
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, profile: profile)
 end
