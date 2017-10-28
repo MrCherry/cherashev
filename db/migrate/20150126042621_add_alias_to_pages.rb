6110f15d5da52c28a588a3b5e006ae9c6595058f
Feodor Cherashev <cherashev.f@gmail.com>
2017-10-28 22:38:02 +0300

    Migrations: Added rails versions

--- db/migrate/20150126042621_add_alias_to_pages.rb
+++ db/migrate/20150126042621_add_alias_to_pages.rb
@@ -1,4 +1,4 @@
-class AddAliasToPages < ActiveRecord::Migration
+class AddAliasToPages < ActiveRecord::Migration[4.2]
   def change
     add_column :pages, :alias, :string, nil: false
     add_index :pages, :alias, unique: true
--- db/migrate/20150206092447_create_file_uploads.rb
+++ db/migrate/20150206092447_create_file_uploads.rb
@@ -1,4 +1,4 @@
-class CreateFileUploads < ActiveRecord::Migration
+class CreateFileUploads < ActiveRecord::Migration[4.2]
   def change
     create_table :file_uploads do |t|
       t.string :original_file_name, null: false
