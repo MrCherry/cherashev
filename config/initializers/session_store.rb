e20e38d388f7cdaee8ee071070f861f94ddaded0
Feodor Cherashev <cherashev.f@gmail.com>
2018-03-25 15:41:36 +0300

    Updated redis configs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,8 @@
 
 # Rails.application.config.session_store :cookie_store, key: '_cherashev_session'
 
-Rails.application.config.session_store :redis_store,
-                                       key: '_cherashev_session',
-                                       redis: Rails.configuration.redis
+Rails.application.config.session_store :redis_store, {
+  servers: [Rails.configuration.redis.merge(namespace: "session")],
+  expire_after: 90.minutes,
+  key: "_#{Rails.application.class.parent_name.downcase}_session"
+}
